79ed02ba4e26159765c858b40b9baba7980aeb28
Brian Semrau <brian@geekkidconsulting.com>
2017-08-04 15:27:12 -0500

    Initial commit

---
+++ Scheduler/Scheduler/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scheduler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now;
+            textBox1.Text = DateTime.Now.Hour.ToString();
+            textBox2.Text = DateTime.Now.Minute.ToString();
+            comboBox1.SelectedIndex = 2;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playsound("0");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playsound("1");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            playsound("2");
+        }
+
+        private void playsound(string num)
+        {
+            try
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Environment.CurrentDirectory + @"\bell" + num + ".wav");
+                player.PlaySync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        
+    }
+}
---
+++ SchoolBell-ring/SchoolBell-ring/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolBell_ring
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string num = "0";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            try
+            {
+                num = Environment.GetCommandLineArgs()[1];
+            }
+            catch
+            {
+                num = "0";
+            }
+            if (num == "")
+            {
+                num = "0";
+            }
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Environment.CurrentDirectory + @"\bell" + num + ".wav");
+            player.PlaySync();
+            Environment.Exit(0);
+            Application.Exit();
+            Close();
+        }
+    }
+}
